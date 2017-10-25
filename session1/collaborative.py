7e86612300b7d6f094f9115000354ffab557b464
Unknown <cvapostol1@sheffield.ac.uk>
2017-10-25 18:34:21 +0100

    Age and hobby
    added a description with age and hobby

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,6 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+age = 22
+hobby = "swimming"
+description = "My age is {} and I like {}.".format(age, hobby)
+print (description)
